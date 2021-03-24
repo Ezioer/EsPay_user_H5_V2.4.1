c3d691f97443714366f87416a43e6f2e56dcb4d3
xiaoqing.zhou <zhouxiaoqing540@gail.com>
2021-03-24 16:47:22 +0800

    添加国家实名认证 version 2.4.9

--- espay_user_lib/src/main/java/com/easou/androidsdk/data/Constant.java
+++ espay_user_lib/src/main/java/com/easou/androidsdk/data/Constant.java
@@ -113,8 +113,8 @@ public class Constant {
     /**
      * H5 SDK url
      */
-    public static String SSO_URL = "http://lab.pay.appeasou.com:7500/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&payHostName=http://lab.pay.appeasou.com&ssoHostName=http://lab.sso.mtianshitong.com";
-//    public static String SSO_URL = "https://h5.pay.mtianshitong.com/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
+    public static String SSO_URL = "http://lab.pay.appeasou.com:7500/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&payHostName=http://lab.pay.appeasou.com&ssoHostName=http://lab.sso.mtianshitong.com&isFcm=true";
+//    public static String SSO_URL = "https://h5.pay.mtianshitong.com/static/sdk/2.0.0/es_sdk2_original.html?1=1&isFcm=true&sdkSource=Android-SDK&";
 
     public static String sso_release = "https://h5.pay.mtianshitong.com/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
     public static String sso_test = "http://lab.pay.appeasou.com:7500/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&payHostName=http://lab.pay.appeasou.com&ssoHostName=http://lab.sso.mtianshitong.com";
