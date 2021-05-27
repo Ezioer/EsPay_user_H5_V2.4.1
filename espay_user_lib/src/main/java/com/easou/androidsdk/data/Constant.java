e0b9511227941538ffedf2eacbe74392313114bd
xiaoqing.zhou <zhouxiaoqing540@gail.com>
2021-05-27 15:00:15 +0800

    更改域名 添加混淆webview的规则，兼容https错误证书的访问问题

--- espay_user_lib/src/main/java/com/easou/androidsdk/data/Constant.java
+++ espay_user_lib/src/main/java/com/easou/androidsdk/data/Constant.java
@@ -15,7 +15,7 @@ public class Constant {
     /**
      * SDK版本号，更新版本记得改版本号
      */
-    public static final String SDK_VERSION = "2.5.1";
+    public static final String SDK_VERSION = "2.5.2";
     public static final String SDK_PHONEOS = "Android";
 
     public static final String API_SOURCE = "30";
@@ -114,7 +114,7 @@ public class Constant {
      * H5 SDK url
      */
 //    public static String SSO_URL = "http://lab.h5.appeasou.com/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&payHostName=http://lab.pay.appeasou.com&ssoHostName=http://lab.sso.mtianshitong.com";
-    public static String SSO_URL = "https://h5.pay.mtianshitong.com/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
+    public static String SSO_URL = "https://h5pay.mtianshitong.com/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
 
     public static String sso_release = "https://h5.pay.mtianshitong.com/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
     public static String sso_test = "http://lab.pay.appeasou.com:7500/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&payHostName=http://lab.pay.appeasou.com&ssoHostName=http://lab.sso.mtianshitong.com";
