b17ac3aef1db96a795d3c99714f99509fb958a46
xiaoqing.zhou <zhouxiaoqing540@gail.com>
2022-03-03 11:35:01 +0800

    修改域名为h5.pay

--- espay_user_lib/src/main/java/com/easou/androidsdk/Starter.java
+++ espay_user_lib/src/main/java/com/easou/androidsdk/Starter.java
@@ -76,7 +76,6 @@ public class Starter {
      * 宜搜SDK登陆接口
      */
     public void login(final Activity activity, ESdkCallback mCallback) {
-        StartOtherPlugin.getOaid(activity);
         Starter.mCallback = mCallback;
         Starter.mActivity = activity;
         StartOtherPlugin.onLaunchApp();
@@ -97,6 +96,7 @@ public class Starter {
                 StartOtherPlugin.logKSActionAppActive();
             }
         }, 3000);
+        StartOtherPlugin.getOaid(activity);
         StartESUserPlugin.loginSdk();
     }
 
