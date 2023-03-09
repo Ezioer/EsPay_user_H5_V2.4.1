f326d5918868ee71b79b73b8513bb22d38a52e94
Ezioer <zhouxiaoqing540@gmail.com>
2023-03-09 13:54:11 +0800

    改为有用户中心的sdk

--- hdtx_user_lib/src/main/java/com/hdtx/androidsdk/data/Constant.java
+++ hdtx_user_lib/src/main/java/com/hdtx/androidsdk/data/Constant.java
@@ -136,8 +136,8 @@ public class Constant {
      */
     public static String URL_BACKUP = "ahudong.cn";
     //    public static String SSO_URL = "http://lab.h5.tjqzqkj.com/static/sdk/3.0.0/es_sdk3_original.html?1=1&sdkSource=Android-SDK&payHostName=http://lab.pay.appeasou.com&ssoHostName=http://lab.sso.mtianshitong.com";
-    public static String SSO_URL = "https://sso.";
-    public static String SSO_REST = "/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
+    public static String SSO_URL = "http://login.";
+    public static String SSO_REST = "/static/sdk/1.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
 
     public static String sso_release = "https://h5.pay.mtianshitong.com/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
     public static String sso_test = "http://lab.pay.appeasou.com:7500/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&payHostName=http://lab.pay.appeasou.com&ssoHostName=http://lab.sso.mtianshitong.com";
