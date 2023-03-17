25e5f524de9315c47eb728f61625456debaea9ee
Ezioer <zhouxiaoqing540@gmail.com>
2023-03-17 14:51:47 +0800

    修复bug

--- espay_user_lib/src/main/java/com/easou/androidsdk/data/Constant.java
+++ espay_user_lib/src/main/java/com/easou/androidsdk/data/Constant.java
@@ -51,7 +51,7 @@ public class Constant {
      */
     public static String OAID = "0";
     public static String qnChannel = "";
-    public static final int isTTVersion = 1;
+    public static final int isTTVersion = 0;
     /**
      * 记录是否为模拟器 0为真机，1为模拟器
      */
@@ -138,8 +138,8 @@ public static String DOMAIN = "https://service.pay.";
      * H5 SDK url
      */
     public static String URL_BACKUP = "mtianshitong.com";
-    //    public static String SSO_URL = "http://lab.h5.tjqzqkj.com/static/sdk/3.0.0/es_sdk3_original.html?1=1&sdkSource=Android-SDK&payHostName=http://lab.pay.appeasou.com&ssoHostName=http://lab.sso.mtianshitong.com";
-    public static String SSO_URL = "https://h5.pay.";
+    public static String SSO_URL = "http://lab.h5.tjqzqkj.com/static/sdk/3.0.0/es_sdk3_original.html?1=1&sdkSource=Android-SDK&payHostName=http://lab.pay.appeasou.com&ssoHostName=http://lab.sso.mtianshitong.com";
+    //    public static String SSO_URL = "https://h5.pay.";
     public static String SSO_REST = "/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
 
     public static String sso_release = "https://h5.pay.mtianshitong.com/static/sdk/2.0.0/es_sdk2_original.html?1=1&sdkSource=Android-SDK&";
